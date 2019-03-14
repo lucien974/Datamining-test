40c7273e0e916a494f7d52524a7977f72f6f36a6
lucien974 <l.cavillot@gmail.com>
2019-03-14 15:42:32 +0100

    Add comments in readline

--- 4_readline.py
+++ 4_readline.py
@@ -21,13 +21,11 @@ dictionnary = []
 classifier = tree.DecisionTreeClassifier("entropy")
 randforest = RandomForestClassifier(n_jobs=2)
 vectorizer = DictVectorizer(sparse = False)
+hasher = FeatureHasher(n_features=2000)
 
 d_training = [{'packed':1, 'contains_encrypted':0}, {'packed':0, 'contains_encrypted':0},
 {'packed':0, 'contains_encrypted':0}, {'packed':1, 'contains_encrypted':1}]
 v_label = [1, 1, 0, 0]
-
-hasher = FeatureHasher(n_features=2000)
-#hashed = hashed[0]
 
 tmp_array = []
 tmp_obj = {}
@@ -37,20 +35,14 @@ limit = 5
 current = 0
 features_name = []
 
+# Extract strings in files
 for tmp_dir in directories:
 	print("directory : ", tmp_dir)
 	onlyfiles = [f for f in listdir(path_to_data + tmp_dir) if isfile(join(path_to_data + tmp_dir, f))]
 	for file in onlyfiles:
 		result = subprocess.Popen(["./convert.sh", tmp_dir + "/" + file], stdout=subprocess.PIPE).communicate()[0]
 		result = result.decode("ascii").split('@')
-		#result = numpy.asarray(result)
-		#if (isinstance(result, list) == False):
-		#	break
-		#print("result : ", result)
-		#print("result[0] : ", result[0])
 		for elem in result:
-			#print("elem : ", elem)
-			#nb = result.count(elem)
 			if elem in tmp_obj:
 				tmp_obj[elem] += 1
 			else:
@@ -61,47 +53,41 @@ for tmp_dir in directories:
 		if (current == cur_directory*limit):
 			break
 		current += 1
-		#print ("current : ", current)
 	tmp_output = 1;
 	cur_directory += 1
 
-#for i in range(2000):
-#	features_name.append(str(i));
-
-#print("data : ", dictionnary)
-
+# Hash the dictionnary
 hashed = hasher.transform(dictionnary)
 hashed = hashed.todense()
 hashed = numpy.asarray(hashed)
 hashed = hashed
 
+# Shuffle the data
 tmp_list = list(zip(hashed, expected_output))
-
 random.shuffle(tmp_list)
-
 hashed, expected_output = zip(*tmp_list)
-
 cut = int(0.8*len(hashed))
+
+# Divide the data
 training_data = hashed[:cut]
 training_labels = expected_output[:cut]
 testing_data = hashed[cut:]
 testing_labels = expected_output[cut:]
 
-#print("hashed : ", hashed)
-
-#vectorizer.fit(hashed)
-
+# Train the classifier
 X = training_data
 y = training_labels
 randforest.fit(X, y)
-#classifier.fit(X, y)
 
+# Save the classifier with pickle
 with open('classifier_strings','wb') as fp:
     pickle.dump(classifier,fp)
 
+# Initialisation for the challenge
 tmp_obj = {}
 tf_do_i_need_this = []
 
+# Predict the challenge data
 test_files = [f for f in listdir(path_to_data + "challenge/") if isfile(join(path_to_data + "challenge/", f))]
 for file in test_files:
 	print("Extraction of ", file, " strings")
@@ -121,10 +107,5 @@ for file in test_files:
 	tmp_hash = tmp_hash.todense()
 	tmp_hash = numpy.asarray(tmp_hash)
 
+# Print the results
 print("results : ", randforest.predict(tmp_hash))
-#with open("classifier.dot", "w") as output_file:
-#    tree.export_graphviz(
-#        classifier,
-#        feature_names=features_name,out_file = output_file)
-
-#os.system("dot classifier.dot -Tpng -o classifier.png")
